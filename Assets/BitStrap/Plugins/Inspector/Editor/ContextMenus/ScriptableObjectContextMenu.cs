12db43a332d78dff53c2e61bfd2905e1ad84b1cc
Johannes Deml <send@johannesdeml.com>
2018-01-22 08:05:20 +0100

    Add context menu to select the scriptable object shown in the inspector
    This actually helps a lot when using scriptable objects extensively

---
+++ Assets/BitStrap/Plugins/Inspector/Editor/ContextMenus/ScriptableObjectContextMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace BitStrap
+{
+	public class ScriptableObjectContextMenu
+	{
+		[MenuItem("CONTEXT/ScriptableObject/Select in Project View")]
+		public static void SelectScriptabelObject(MenuCommand menuCommand)
+		{
+			EditorGUIUtility.PingObject(menuCommand.context);
+		}
+	}
+}
