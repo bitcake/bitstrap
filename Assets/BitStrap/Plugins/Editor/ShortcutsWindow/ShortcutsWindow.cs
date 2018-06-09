460ecde79e2961b77b9d540a67aae0cf3040c9d1
Matheus Lessa <matheuslessarodrigues@gmail.com>
2018-06-09 18:38:12 -0300

    basic selection stuff

--- Assets/BitStrap/Plugins/Editor/ShortcutsWindow/ShortcutsWindow.cs
+++ Assets/BitStrap/Plugins/Editor/ShortcutsWindow/ShortcutsWindow.cs
@@ -5,6 +5,11 @@ namespace BitStrap
 {
 	public sealed class ShortcutsWindow : EditorWindow
 	{
+		private static class Colors
+		{
+			public static readonly Color SelectionColor = new Color32( 24, 118, 206, 255 );
+		}
+
 		private const string UndoString = "ShortcutsWindow_Undo";
 
 		private Option<ShortcutsReferences> shortcutsReferences;
@@ -12,7 +17,7 @@ namespace BitStrap
 		[MenuItem( "Window/BitStrap/Shortcuts Window" )]
 		private static void OpenWindow()
 		{
-			var window = GetWindow<ShortcutsWindow>( typeof( ShortcutsWindow ).Name );
+			var window = GetWindow<ShortcutsWindow>( "ShortcutsWindow" );
 			window.Show();
 		}
 
@@ -20,27 +25,82 @@ namespace BitStrap
 		{
 			ShortcutsReferences references;
 			if( !shortcutsReferences.OrElse( () => AssetDatabaseHelper.FindAssetOfType<ShortcutsReferences>() ).TryGet( out references ) )
-			{
 				return;
+
+			var areaRect = new Rect( position );
+			areaRect.position = Vector2.zero;
+			GUILayout.BeginArea( areaRect );
+
+			const float padding = 8.0f;
+			const float margin = 4.0f;
+
+			var clickCount = Event.current.clickCount;
+			var mousePosition = Event.current.mousePosition;
+
+			for( int i = 0; i < references.references.Length; i++ )
+			{
+				var r = references.references[i];
+
+				var previewTexture = AssetPreview.GetMiniThumbnail( r );
+				var width = previewTexture.width + padding;
+				var previewHeight = previewTexture.height;
+				var labelHeight = EditorGUIUtility.singleLineHeight;
+				var x = i * ( width + margin ) + padding * 0.5f;
+				var y = 0.0f;
+
+				var totalRect = new Rect( x, y, width, previewHeight + labelHeight );
+				var totalRectAbsolute = new Rect( totalRect );
+				totalRectAbsolute.position = position.position;
+
+				if( clickCount > 0 && totalRect.Contains( mousePosition ) )
+				{
+					Selection.activeObject = r;
+					Repaint();
+				}
+
+				bool isSelected = Selection.Contains( r );
+				if( isSelected )
+					EditorGUI.DrawRect( totalRect, Colors.SelectionColor );
+
+				var previewRect = new Rect( x, y, width, previewHeight );
+				GUI.Label( previewRect, previewTexture );
+
+				var labelRect = new Rect( x, y + previewHeight, width, labelHeight );
+				var labelStyle = isSelected ? EditorStyles.whiteLabel : EditorStyles.label;
+				GUI.Label( labelRect, r.name, labelStyle );
 			}
+			GUILayout.EndArea();
+			return;
 
 			var indexToDelete = -1;
 			for( int i = 0; i < references.references.Length; i++ )
 			{
 				var r = references.references[i];
+				var previewTexture = AssetPreview.GetMiniThumbnail( r );
+				var width = previewTexture.width;
 
-				using( Horizontal.Do() )
+				using( Horizontal.Do( GUILayout.Width( width ) ) )
 				{
-					if( GUILayout.Button( r.name ) )
+					using( Vertical.Do() )
 					{
-						Selection.activeObject = r;
-						EditorGUIUtility.PingObject( r );
+						GUILayout.Label( previewTexture );
+						GUILayout.Label( r.name, GUILayout.Width( width ) );
+
+						//*
+						if( GUILayout.Button( previewTexture, EditorStyles.label ) )
+						{
+							Selection.activeObject = r;
+							EditorGUIUtility.PingObject( r );
+						}
+						//*/
 					}
 
+					//*
 					GUI.backgroundColor = Color.red;
 					if( GUILayout.Button( "X", GUILayout.Width( 32.0f ) ) )
 						indexToDelete = i;
 					GUI.backgroundColor = Color.white;
+					//*/
 				}
 			}
 
@@ -61,6 +121,7 @@ namespace BitStrap
 				if( eventType == EventType.DragPerform )
 				{
 					DragAndDrop.AcceptDrag();
+					Undo.RecordObject( references, UndoString );
 
 					foreach( var o in DragAndDrop.objectReferences )
 					{
