eca19cf75a81108e001ca12e7240d7fa0d922719
Luiz Wendt <luizaugustow_1@msn.com>
2018-08-10 00:23:24 -0300

    Implemented Android Key Action

---
+++ Assets/BitStrap/Plugins/Editor/UMake/BuildActions/Scripts/AndroidKeyAction.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace BitStrap
+{
+    public sealed class AndroidKeyAction : UMakeBuildAction
+    {
+        public string keyStoreName = "user.keystore";
+        public string keyStorePassword = "keyStorePassword";
+        public string keyAliasName = "aliasName";
+        public string keyAliasPassword = "keyAliasPassword";
+
+        private static string ProjectPath
+        {
+            get
+            {
+                return Application.dataPath.Remove(Application.dataPath.Length - 6, 6);
+            }
+        }
+
+        public override void Execute(UMake umake, UMakeTarget target)
+        {
+            PlayerSettings.keystorePass = keyStorePassword;
+            PlayerSettings.keyaliasPass = keyAliasPassword;
+
+            PlayerSettings.Android.keystoreName = ProjectPath + keyStoreName;
+            PlayerSettings.Android.keyaliasName = keyAliasName;
+        }
+    }
+}
