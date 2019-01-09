ef53ff19db34654325fc8f8c8da5269663fec37a
Luiz Augusto Wendt <luiz.wendt@outlook.com>
2019-01-08 22:00:55 -0200

    Implemented Semantic Versioning

---
+++ Assets/BitStrap/Plugins/Editor/UMake/BuildActions/Scripts/IncrementSemanticVersioningBuildAction.cs
@@ -0,0 +1,62 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace BitStrap
+{
+	/// <summary>
+	/// Version of <see cref="IncrementVersionBuildAction"/> but based on Semantic Versioning(https://semver.org/)
+	/// </summary>
+	public sealed class IncrementSemanticVersioningBuildAction : UMakeBuildAction
+	{
+		public enum SemanticVersionType
+		{
+			Major,
+			Minor,
+			Patch,
+			Dev
+		}
+
+		public char separator = '.';
+		public SemanticVersionType increaseVersionType = SemanticVersionType.Patch;
+		public bool updateApplicationVersion = false;
+
+		public override void Execute(UMake umake, UMakeTarget target)
+		{
+			Undo.RecordObject(umake, "UMakeBuildAction");
+
+			string[] parts = umake.version.Split(separator);
+			int version = 1;
+			int index = (int)increaseVersionType;
+
+			if (index >= parts.Length)
+			{
+				string[] versions = new string[(index + 1) - parts.Length];
+				for(int i = 0; i < versions.Length; i++)
+				{
+					versions[i] = "0";
+				}
+				ArrayUtility.AddRange(ref parts, versions);
+			}
+			else if (int.TryParse(parts[index], out version))
+			{
+				version += 1;
+			}
+
+			//remove dev version if is not dev
+			if (increaseVersionType != SemanticVersionType.Dev && parts.Length == 4)
+			{
+				ArrayUtility.RemoveAt(ref parts, 3);
+			}
+
+			parts[index] = version.ToString();
+			umake.version = string.Join(separator.ToString(), parts);
+
+			if (updateApplicationVersion)
+			{
+				PlayerSettings.bundleVersion = umake.version;
+				Debug.Log("The application is now with version " + Application.version);
+			}
+			EditorUtility.SetDirty(umake);
+		}
+	}
+}
