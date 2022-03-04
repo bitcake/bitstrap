b01fe712618f09d5cd942a0ec1517c0a6c9a55cc
elamhut <elamhut@gmail.com>
2022-03-03 21:11:10 -0300

    Now you cannot change folder name!

--- Assets/Editor/BitPipe/FolderStructure.cs
+++ Assets/Editor/BitPipe/FolderStructure.cs
@@ -1,131 +1,134 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
-using Application = UnityEngine.Application;
 
-[System.Serializable]
-public class BitFolder
+
+namespace BitStrap
 {
-    public string folderName;
-    [NonSerialized] public bool isExpanded = true;
-    [NonSerialized] public bool markedForDeletion;
-    public List<BitFolder> childFolders = new();
-}
-
-public class FolderStructure : EditorWindow
-{
-    private const string jsonPath = "Editor/BitPipe/project_structure.json";
-    public BitFolder bitFolder;
-    private TextAsset jsonAsset;
-
-    private SerializedObject so;
-
-    [MenuItem( "Window/BitStrap/BitPipe" )]
-    public static void OpenFolderStructure() => GetWindow<FolderStructure>();
-
-    private void OnEnable()
+    [System.Serializable]
+    public class BitFolder
     {
-        so = new SerializedObject( this );
-        bitFolder = new BitFolder();
-        jsonAsset = AssetDatabase.LoadAssetAtPath<TextAsset>( "Assets/" + jsonPath );
-        if( jsonAsset == null )
-        {
-            File.WriteAllText( Path.Combine( Application.dataPath, jsonPath ), "{}" );
-            AssetDatabase.ImportAsset( "Assets/" + jsonPath );
-            jsonAsset = AssetDatabase.LoadAssetAtPath<TextAsset>( "Assets/" + jsonPath );
-        }
-
-        bitFolder = JsonUtility.FromJson<BitFolder>( jsonAsset.text );
+        public string folderName;
+        [NonSerialized] public bool isExpanded = true;
+        [NonSerialized] public bool markedForDeletion;
+        public List<BitFolder> childFolders = new();
     }
 
-    private void DrawBitFolder( BitFolder bitFolder )
+    public class FolderStructure : EditorWindow
     {
-        EditorGUILayout.BeginHorizontal();
-        bitFolder.isExpanded =
-            EditorGUILayout.Toggle( bitFolder.isExpanded, EditorStyles.foldoutHeader, GUILayout.Width( 16 ) );
+        private const string jsonPath = "Editor/BitPipe/project_structure.json";
+        private const string jsonRelativePath = "Assets/" + jsonPath;
+        public BitFolder bitFolder;
+        private TextAsset jsonAsset;
 
-        // TextField for typing the name of the folders, delete any added WhiteSpaces
-        bitFolder.folderName = EditorGUILayout.DelayedTextField( bitFolder.folderName );
-        if( !string.IsNullOrEmpty( bitFolder.folderName ) )
-            bitFolder.folderName = bitFolder.folderName.Replace( " ", "" );
-        
-        if( GUILayout.Button( "+", GUILayout.Width( 25 ) ) )
+        private SerializedObject so;
+
+        [MenuItem( "Window/BitStrap/BitPipe" )]
+        public static void OpenFolderStructure() => GetWindow<FolderStructure>();
+
+        private void OnEnable()
         {
-            bitFolder.childFolders.Add( new BitFolder() );
-        }
-
-        if( GUILayout.Button( "-", GUILayout.Width( 25 ) ) )
-        {
-            if( EditorUtility.DisplayDialog( "Are you sure?", $"Do you REALLY want to delete {bitFolder.folderName}?",
-                "Yes", "No" ) )
-                bitFolder.markedForDeletion = true;
-        }
-
-        EditorGUILayout.EndHorizontal();
-        EditorGUI.indentLevel++;
-
-        if( bitFolder.isExpanded )
-        {
-            foreach( var folder in bitFolder.childFolders )
+            so = new SerializedObject( this );
+            jsonAsset = AssetDatabase.LoadAssetAtPath<TextAsset>( jsonRelativePath );
+            if( jsonAsset == null )
             {
-                DrawBitFolder( folder );
+                File.WriteAllText( Path.Combine( Application.dataPath, jsonPath ), "{}" );
+                AssetDatabase.ImportAsset( jsonRelativePath );
+                jsonAsset = AssetDatabase.LoadAssetAtPath<TextAsset>( jsonRelativePath );
+            }
+
+            bitFolder = JsonUtility.FromJson<BitFolder>( jsonAsset.text );
+        }
+
+        private void DrawBitFolder( BitFolder bitFolder )
+        {
+            EditorGUILayout.BeginHorizontal();
+            bitFolder.isExpanded =
+                EditorGUILayout.Toggle( bitFolder.isExpanded, EditorStyles.foldoutHeader, GUILayout.Width( 16 ) );
+
+            // TextField for typing the name of the folders, delete any added WhiteSpaces
+            bitFolder.folderName = EditorGUILayout.DelayedTextField( bitFolder.folderName );
+            if( !string.IsNullOrEmpty( bitFolder.folderName ) )
+                bitFolder.folderName = bitFolder.folderName.Replace( " ", "" );
+
+            if( GUILayout.Button( "+", GUILayout.Width( 25 ) ) )
+            {
+                bitFolder.childFolders.Add( new BitFolder() );
+            }
+
+            if( GUILayout.Button( "-", GUILayout.Width( 25 ) ) )
+            {
+                if( EditorUtility.DisplayDialog( "Are you sure?",
+                    $"Do you REALLY want to delete {bitFolder.folderName}?",
+                    "Yes", "No" ) )
+                    bitFolder.markedForDeletion = true;
+            }
+
+            EditorGUILayout.EndHorizontal();
+            EditorGUI.indentLevel++;
+
+            if( bitFolder.isExpanded )
+            {
+                foreach( var folder in bitFolder.childFolders )
+                {
+                    DrawBitFolder( folder );
+                }
+            }
+
+            for( var index = bitFolder.childFolders.Count - 1; index >= 0; index-- )
+            {
+                var folder = bitFolder.childFolders[index];
+                if( folder.markedForDeletion )
+                    bitFolder.childFolders.Remove( folder );
+            }
+
+            EditorGUI.indentLevel--;
+        }
+
+        private void OnGUI()
+        {
+            so.Update();
+            EditorGUILayout.LabelField( "BitPipe", EditorStyles.largeLabel );
+            EditorGUI.BeginChangeCheck();
+            DrawBitFolder( bitFolder );
+            if( EditorGUI.EndChangeCheck() )
+            {
+                UpdateJson();
+            }
+
+            GUILayout.Space( 20 );
+            if( GUILayout.Button( "Generate Folders", GUILayout.Height( 35 ) ) )
+            {
+                // if( EditorUtility.DisplayDialog( "Are you sure?", $"This will generate empty folders in your project.",
+                //     "Yes", "No" ) )
+                GenerateFolders( bitFolder, Application.dataPath );
+                AssetDatabase.Refresh();
+            }
+
+            so.ApplyModifiedProperties();
+        }
+
+        private void GenerateFolders( BitFolder folder, string parentPath )
+        {
+            var path = parentPath + "/" + folder.folderName;
+            // Debug.Log( "Generated Path: " + path );
+            if( !Directory.Exists( path ) )
+                Directory.CreateDirectory( path );
+
+            foreach( var childFolder in folder.childFolders )
+            {
+                GenerateFolders( childFolder, path );
             }
         }
 
-        for( var index = bitFolder.childFolders.Count - 1; index >= 0; index-- )
+        private void UpdateJson()
         {
-            var folder = bitFolder.childFolders[index];
-            if( folder.markedForDeletion )
-                bitFolder.childFolders.Remove( folder );
+            File.WriteAllText( AssetDatabase.GetAssetPath( jsonAsset ), JsonUtility.ToJson( bitFolder, true ) );
+            EditorUtility.SetDirty( jsonAsset );
+            AssetDatabase.SaveAssets();
+            AssetDatabase.ImportAsset( "Assets/" + jsonPath );
         }
-
-        EditorGUI.indentLevel--;
-    }
-
-    private void OnGUI()
-    {
-        so.Update();
-        EditorGUILayout.LabelField( "BitPipe", EditorStyles.largeLabel );
-        EditorGUI.BeginChangeCheck();
-        DrawBitFolder( bitFolder );
-        if( EditorGUI.EndChangeCheck() )
-        {
-            UpdateJson();
-        }
-
-        GUILayout.Space( 20 );
-        if( GUILayout.Button( "Generate Folders", GUILayout.Height( 35 ) ) )
-        {
-            // if( EditorUtility.DisplayDialog( "Are you sure?", $"This will generate empty folders in your project.",
-            //     "Yes", "No" ) )
-            GenerateFolders( bitFolder, Application.dataPath );
-            AssetDatabase.Refresh();
-        }
-
-        so.ApplyModifiedProperties();
-    }
-
-    private void GenerateFolders( BitFolder folder, string parentPath )
-    {
-        var path = parentPath + "/" + folder.folderName;
-        // Debug.Log( "Generated Path: " + path );
-        if( !Directory.Exists( path ) )
-            Directory.CreateDirectory( path );
-
-        foreach( var childFolder in folder.childFolders )
-        {
-            GenerateFolders( childFolder, path );
-        }
-    }
-
-    private void UpdateJson()
-    {
-        File.WriteAllText( AssetDatabase.GetAssetPath( jsonAsset ), JsonUtility.ToJson( bitFolder, true ) );
-        EditorUtility.SetDirty( jsonAsset );
-        AssetDatabase.SaveAssets();
-        AssetDatabase.ImportAsset( "Assets/" + jsonPath );
     }
 }
--- Assets/Editor/BitPipe/FolderStructureMaintainer.cs
+++ Assets/Editor/BitPipe/FolderStructureMaintainer.cs
@@ -1,94 +1,40 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using UnityEditor;
-using UnityEditor.VersionControl;
 using UnityEngine;
 
-public class FolderStructureMaintainer : AssetPostprocessor
+namespace BitStrap
 {
-    private const string jsonPath = "Assets/Editor/BitPipe/project_structure.json";
-    
-    private static void OnPostprocessAllAssets( string[] importedAssets, string[] deletedAssets, string[] movedAssets,
-        string[] movedFromAssetPaths )
+    public class FolderStructureMaintainer : AssetPostprocessor
     {
-        foreach( var asset in movedAssets )
+        private const string jsonPath = "Assets/Editor/BitPipe/project_structure.json";
+
+        private void OnPreprocessAsset()
         {
+            Debug.Log( assetPath );
+
             // Checks if asset is a file or directory
-            FileAttributes attr = File.GetAttributes( asset );
+            FileAttributes attr = File.GetAttributes( assetPath );
             if( ( attr & FileAttributes.Directory ) == FileAttributes.Directory )
             {
-                // Debug.Log( $"Asset que foi modificado {asset}" );
                 var jsonAsset = AssetDatabase.LoadAssetAtPath<TextAsset>( jsonPath );
                 var bitFolder = JsonUtility.FromJson<BitFolder>( jsonAsset.text );
-                if( CompareFolders( bitFolder, "Assets", asset ) )
-                {
-                    Debug.Log( "Renomeiandow" );
-                    break;
-                }
+                CompareFolders( bitFolder, "Assets", assetPath );
             }
         }
-    }
 
-    static bool CompareFolders( BitFolder bitFolder, string parentPath, string modifiedAssetPath )
-    {
-        var constructedBitfolderPath = parentPath + "/" + bitFolder.folderName;
-        var leafModifiedAssetPath = modifiedAssetPath.Split( "/" ).Last();
-        
-        if(!Directory.Exists( constructedBitfolderPath ))
+
+        static void CompareFolders( BitFolder bitFolder, string parentPath, string modifiedAssetPath )
         {
-            // The Hacking below is because Unity isn't reliable
-            // Sometimes it'll accuse the Child folders as the ones being renamed
-            
-            Debug.Log( $"Folder Name: {bitFolder.folderName} and Lead Dir: {leafModifiedAssetPath}" );
-            
-            var splitParentPath = constructedBitfolderPath.Split( "/" );
-            var splitModifiedAssetPath = modifiedAssetPath.Split( "/" );
-            List<String> finalWrongPath = new List<string>();
-            List<String> finalCorrectPath = new List<string>();
-            string constructedFinalWrongPath = null;
-            string constructedFinalCorrectPath = null;
-            
-            if(bitFolder.folderName == leafModifiedAssetPath)
+            var folderPath = Path.Join( parentPath, bitFolder.folderName );
+
+            if( !Directory.Exists( folderPath ) )
             {
-                for( int i = 0; i < splitParentPath.Length; i++ )
-                {
-                    if( splitParentPath[i] == splitModifiedAssetPath[i] )
-                    {
-                        finalWrongPath.Add( splitModifiedAssetPath[i] );
-                        finalCorrectPath.Add( splitParentPath[i] );
-                    }
-                    else
-                    {
-                        finalWrongPath.Add( splitModifiedAssetPath[i] );
-                        finalCorrectPath.Add( splitParentPath[i] );
-                        constructedFinalWrongPath = string.Join( "/", finalWrongPath.ToArray() );
-                        constructedFinalCorrectPath= string.Join( "/", finalCorrectPath.ToArray() );
-                        break;
-                    }
-                }
-                
-                Debug.LogError( $" Correct Path: {constructedFinalCorrectPath} | Wrong Path: {constructedFinalWrongPath} " );
-                RenameAndDeleteDirs( constructedFinalWrongPath, constructedFinalCorrectPath );
-                return true;
+                Undo.PerformUndo();
+                return;
             }
+            foreach( var childFolder in bitFolder.childFolders )
+                CompareFolders( childFolder, folderPath, modifiedAssetPath );
         }
-
-        foreach( var childFolder in bitFolder.childFolders )
-        {
-            if( CompareFolders( childFolder, constructedBitfolderPath, modifiedAssetPath ) )
-                return true;
-        }
-        return false;
     }
-
-    private static void RenameAndDeleteDirs( string constructedFinalWrongPath, string constructedFinalCorrectPath )
-    {
-        FileUtil.MoveFileOrDirectory( constructedFinalWrongPath, constructedFinalCorrectPath );
-        FileUtil.DeleteFileOrDirectory( constructedFinalWrongPath + ".meta" );
-        FileUtil.DeleteFileOrDirectory( constructedFinalWrongPath );
-        AssetDatabase.Refresh();
-    }
+    
 }
