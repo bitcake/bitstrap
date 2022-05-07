7f9983668ab44ee1ec7d01a6682590e97caa006d
elamhut <elamhut@gmail.com>
2022-05-07 00:32:42 -0300

    Now it's possible to enable or disable specific scene creation

--- Assets/Editor/BitPipe/BitPipeSettings.cs
+++ Assets/Editor/BitPipe/BitPipeSettings.cs
@@ -1,16 +1,46 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace BitStrap
 {
     public sealed class BitPipeSettings : ScriptableObject
     {
-        public static string persistentSceneName = "P";
-        public static string gameplaySceneName = "Gameplay";
-        public static string geometrySceneName = "Geometry";
-        public static string lightingSceneName = "Lighting";
-        public static string audioSceneName = "Audio";
+        public static string sPersistentSceneName = "P";
+        public static string sGameplaySceneName = "Gameplay";
+        public static string sGeometrySceneName = "Geometry";
+        public static string sLightingSceneName = "Lighting";
+        public static string sAudioSceneName = "Audio";
+
+        public bool createPersistentScene = true;
+        public string persistentSceneName = "P";
+        public bool createGameplayScene;
+        public string gameplaySceneName = "Gameplay";
+        public bool createGeometryScene;
+        public string geometrySceneName = "Geometry";
+        public bool createLightingScene;
+        public string lightingSceneName = "Lighting";
+        public bool createAudioScene;
+        public string audioSceneName = "Audio";
 
         public static string[] sceneTypeNames = new[]
-            { persistentSceneName, gameplaySceneName, geometrySceneName, lightingSceneName, audioSceneName };
+            { sPersistentSceneName, sGameplaySceneName, sGeometrySceneName, sLightingSceneName, sAudioSceneName };
+
+        public List<string> GetSceneTypesToCreate()
+        {
+            List<string> sceneTypes = new List<string>();
+
+            if( createPersistentScene )
+                sceneTypes.Add( persistentSceneName );
+            if( createGameplayScene )
+                sceneTypes.Add( gameplaySceneName );
+            if( createGeometryScene )
+                sceneTypes.Add( geometrySceneName );
+            if( createLightingScene )
+                sceneTypes.Add( lightingSceneName );
+            if( createAudioScene )
+                sceneTypes.Add( audioSceneName );
+
+            return sceneTypes;
+        }
     }
 }
